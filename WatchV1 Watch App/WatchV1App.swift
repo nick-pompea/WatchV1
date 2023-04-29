4ea5ab4a8c6ffd9137f37624b0a984fc7b5e376d
Nicholas Pompea <nick.pompea@gmail,com>
2023-04-28 21:11:35 -0600

    Initial Commit

---
+++ WatchV1 Watch App/WatchV1App.swift
@@ -0,0 +1,17 @@
+//
+//  WatchV1App.swift
+//  WatchV1 Watch App
+//
+//  Created by Nicholas Pompea on 4/28/23.
+//
+
+import SwiftUI
+
+@main
+struct WatchV1_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
